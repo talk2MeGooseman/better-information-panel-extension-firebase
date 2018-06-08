efcf24b832aaa661a3f67725878ac0a43ca502aa
Erik Guzman <erik.guzman@guzman.codes>
2018-06-08 12:16:31 -0700

    Update constants

--- functions_es6/src/Constants.js
+++ functions_es6/src/Constants.js
@@ -1,6 +1,6 @@
-export const EXTENSTION_USER_ID = "TWITCH_USER_ID";
+export const EXTENSTION_USER_ID = "120750024";
 export const JWT_HEADER = 'x-extension-jwt';
 export const TWITCH_BASE_EXTENSION_URL = 'https://api.twitch.tv/extensions';
 export const EXTENSION_ID = 'pp16i51f77pxhwls8tqqo72f5m5ttz';
 export const CONFIG_KEY = 'CONFIG_KEY';
-export const EXTENSION_VERSION = 'EXTENSION_VERSION';
+export const EXTENSION_VERSION = '0.0.1';
